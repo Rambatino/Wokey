536d260f89505c292c855bce265f0fb3979bfd29
Mark Ramotowski <mark.tint.ramotowski@gmail.com>
2019-04-24 12:39:17 +1200

    Removed scroll indicators

--- src/containers/Space.tsx
+++ src/containers/Space.tsx
@@ -3,7 +3,7 @@ import LinkedCardItem from '../components/LinkedCardItem'
 import BaseCardItem from '../components/BaseCardItem'
 import './containers.scss'
 
-import { ArcherContainer } from 'react-archer'
+import { ArcherContainer, ArcherContainerProps } from 'react-archer'
 
 import { BaseCard, LinkedCard } from '../store/types'
 
@@ -13,18 +13,46 @@ type Props = {
     baseCards: Array<BaseCard>
   }
 }
-export default class Space extends Component<Props> {
+type State = {
+  currentSelectionIdx: number
+}
+export default class Space extends Component<Props, State> {
   state = {
     currentSelectionIdx: 0,
+  }
+  baseSpaceRef = React.createRef<HTMLDivElement>()
+  linkSpaceRef = React.createRef<HTMLDivElement>()
+  archerContainerRef = React.createRef<ArcherContainerProps>()
+
+  componentDidMount = () => {
+    this.baseSpaceRef.current &&
+      this.baseSpaceRef.current.addEventListener('scroll', this.isScrolling)
+    this.linkSpaceRef.current &&
+      this.linkSpaceRef.current.addEventListener('scroll', this.isScrolling)
+  }
+
+  componentWillUnmount = () => {
+    this.baseSpaceRef.current &&
+      this.baseSpaceRef.current.removeEventListener('scroll', this.isScrolling)
+    this.linkSpaceRef.current &&
+      this.linkSpaceRef.current.removeEventListener('scroll', this.isScrolling)
+  }
+
+  isScrolling = () => {
+    this.archerContainerRef!.current!.forceUpdate()
   }
 
   render() {
     return (
       <div>
-        <ArcherContainer>
+        <ArcherContainer ref={this.archerContainerRef}>
           <div className="space">
-            <div className="baseSpace">{this.baseCards()}</div>
-            <div className="linkedSpace">{this.linkedCards()}</div>
+            <div className="baseSpace" ref={this.baseSpaceRef}>
+              {this.baseCards()}
+            </div>
+            <div className="linkedSpace" ref={this.linkSpaceRef}>
+              {this.linkedCards()}
+            </div>
           </div>
         </ArcherContainer>
       </div>
