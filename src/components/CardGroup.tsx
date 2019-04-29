8129c9babade39880d974df2b319c832174469bb
Mark Ramotowski <mark.tint.ramotowski@gmail.com>
2019-04-29 15:00:02 +1200

    Working visuals

--- src/components/CardGroup.tsx
+++ src/components/CardGroup.tsx
@@ -8,25 +8,22 @@ type Props = {
   data: BaseCard
   children?: never
 }
+
 export default class CardGroup extends Component<Props> {
   render() {
-    console.log(this.props.data.linkedCards)
     return (
       <div className="CardGroup" onClick={this.props.onClick}>
+        {this.props.data.linkedCards.length === 0 ? (
+          <div className="contentTitle" style={{ width: 200 }}>
+            {this.props.data.item.title}
+          </div>
+        ) : (
+          <div className="contentTitle">{this.props.data.item.title}</div>
+        )}
+        <hr />
         <div className="cards">
           <div className="content">
-            <p className="title">{this.props.data.item.title}</p>
             <p className="subtitle">{this.props.data.item.subtitle}</p>
-            {this.props.data.item.descHtml ? (
-              <p
-                className="desc"
-                dangerouslySetInnerHTML={{
-                  __html: this.props.data.item.descHtml,
-                }}
-              />
-            ) : (
-              <p className="desc"> {this.props.data.item.desc}</p>
-            )}
             <div className="state">{this.props.data.item.state}</div>
           </div>
           {this.props.data.linkedCards.map((card, i) => (
